1e1c467a34279223e1f1304aa749d9ab40128833
fusserg007 <126871571+fusserg007@users.noreply.github.com>
2025-08-25 12:58:39 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-# ВНИМАНИЕ: Замените эти значения на ваши реальные данные перед использованием
-API_TOKEN = 'YOUR_BOT_TOKEN_HERE'
-CHANNEL_ID = -1001234567890  # Замените на ID вашего канала
-ADMIN_ID = 123456789  # Замените на ваш Telegram ID
-
-TEMPLATE_FILE = 'template.json'
